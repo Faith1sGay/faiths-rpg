241b164b62db66f74039e722617815fd33bcff53
Faith1sGay <daniel.viola001@mymdc.net>
2021-02-07 17:13:19 -0500

    delete files i uploaded yesterday in frustation

--- Game/interface/data/dataLoader.js
+++
@@ -1,5 +0,0 @@
-import YAML from 'yaml';
-import * as Paths from 'path';
-import * as fs from 'fs';
-const data = fs.readFileSync(Paths.resolve('./interface/data/data.yml'));
-export const gameInterface = YAML.parse(data.toString());
